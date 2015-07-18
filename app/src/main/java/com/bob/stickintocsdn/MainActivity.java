46282ce5af08c8c80cefc5ed68746f5c5cf49ec2
bob <bob1993.dev@gmail.com>
2015-07-18 11:47:48 +0800

    优化网络访问逻辑，提高并发度。重构代码

--- app/src/main/java/com/bob/stickintocsdn/MainActivity.java
+++ app/src/main/java/com/bob/stickintocsdn/MainActivity.java
@@ -1,7 +1,7 @@
 package com.bob.stickintocsdn;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -12,14 +12,14 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.bob.stickintocsdn.utils.Constants;
 
 public class MainActivity extends AppCompatActivity {
-    private RequestQueue queue;
     private EditText blogUrlEt;
     private EditText timesEt;
     private ImageView sendBt;
     private String url;//博客链接
-    private int times;//刷博客次数
+    private int TotalTimes;//刷博客次数
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,36 +29,42 @@ public class MainActivity extends AppCompatActivity {
         sendBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                url= "http://blog.csdn.net/bob1993_dev/article/details/";//博客链接
-                url= url+blogUrlEt.getText().toString();
-                //Log.i("url", url);
-                times= Integer.parseInt(timesEt.getText().toString());
-                for (int i= 0;i< times;i++){
-
-                    StringRequest request= new StringRequest(url, new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String s) {
-                           // Log.i("result", "success");
-                        }
-                    }, new Response.ErrorListener(){
-                        @Override
-                        public void onErrorResponse(VolleyError volleyError) {
-                            Log.i("result", "failed");
-                        }
-                    });
-
-                    queue.add(request);
+                url = Constants.ARTICLE_URL;
+                TotalTimes = Integer.parseInt(timesEt.getText().toString());
+                for (int i = 0; i < TotalTimes / 1000; i++) {//按照一千条一组，分x组进行并发访问
+                    doTask(blogUrlEt.getText().toString(), 1000);
                 }
-
+                doTask(blogUrlEt.getText().toString(), TotalTimes % 1000);
             }
         });
 
     }
 
-    private void init(){
-        queue= Volley.newRequestQueue(this);
+    private void doTask(String articleId, int times) {
+        RequestQueue queue= Volley.newRequestQueue(this);
+        url = url + articleId;
+        Log.i("url", url);
+        for (int i = 0; i < times; i++) {
+
+            StringRequest request = new StringRequest(url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String s) {
+                    // Log.i("result", "success");
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError volleyError) {
+                    Log.i("result", "failed");
+                }
+            });
+
+            queue.add(request);
+        }
+    }
+
+    private void init() {
         blogUrlEt = (EditText) findViewById(R.id.et_url);
         timesEt = (EditText) findViewById(R.id.et_times);
-        sendBt= (ImageView)findViewById(R.id.bt_send);
+        sendBt = (ImageView) findViewById(R.id.bt_send);
     }
 }
---
+++ app/src/main/java/com/bob/stickintocsdn/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.bob.stickintocsdn.utils;
+
+/**
+ * Created by Bob on 15/7/18.
+ */
+public class Constants {
+    public static String ARTICLE_URL="http://blog.csdn.net/bob1993_dev/article/details/";
+}
